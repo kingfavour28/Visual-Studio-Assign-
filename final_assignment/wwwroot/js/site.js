a46d5b5d4422520f602e7bbc0c17ed93d54087cb
kingfavour28 <fumogbai@yahoo.com>
2024-06-20 20:13:14 -0400

    Adding new files

---
+++ final_assignment/wwwroot/js/site.js
@@ -0,0 +1,13 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+document.querySelectorAll(".text").forEach((elem) => {
+    elem.addEventListener("mouseenter", (event) => {
+        event.target.style.color = "white";
+    });
+
+    elem.addEventListener("mouseleave", (event) => {
+        event.target.style.color = "black";
+    });
+});
